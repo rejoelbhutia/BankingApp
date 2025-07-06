4bb38306e876fc9997f3b556f2ee483cd51e7794
Abash Ansari <ansariabash2004@gmail.com>
2025-07-06 06:13:52 +0530

    file for creating customer when "Create Account" button is clicked

---
+++ backend/CreateAccount.java
@@ -0,0 +1,16 @@
+package backend;
+import CreateAccountScreen;
+import java.awt.event.*; 
+
+public class CreateAccount {
+    public static void main(String[] args) {
+        //Event Listener
+    createButton.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+        String userName = userName.getText();
+        String gender = gender.getText();
+        String email = emailField.getText();
+    }
+  })
+ }
+}
