16ba9b6a2ffda8387c61036dc079176d265ffcdc
Abash Ansari <ansariabash2004@gmail.com>
2025-07-06 11:54:44 +0530

    added an object to save customerDetails in "CustomersDetails.txt" file using "FileWriter" class in java.io package

--- backend/CreateAccount.java
+++ backend/CreateAccount.java
@@ -1,6 +1,10 @@
 package backend;
 import CreateAccountScreen;
-import java.awt.event.*; 
+import java.awt.event.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane; 
 
 public class CreateAccount {
     public static void main(String[] args) {
@@ -12,5 +16,13 @@ public class CreateAccount {
         String email = emailField.getText();
     }
   })
+    try {
+        FileWriter writer = new FileWriter("CustomersDetails.txt");
+        writer.write("userName: "+ userName +", gender: "+ gender +", email: "+ email +"\n");
+        writer.close();
+        JOptionPane.showMessageDialog(frame,"Account saved!"); // display a pop-up dialog box
+    } catch (IOException error) {
+        error.printStackTrace();
+    }
  }
 }
